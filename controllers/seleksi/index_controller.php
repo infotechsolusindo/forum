0323c993c36355a787d44fca719bb5ec47d64944
webmaster <webmaster@metalogic.com>
2017-07-12 20:52:06 +0700

    - landing page for peserta

--- controllers/seleksi/index_controller.php
+++ controllers/seleksi/index_controller.php
@@ -4,40 +4,43 @@
  */
 class Index_Controller extends Controller {
 
-	function __construct() {
-		parent::__construct();
-		$header = new View();
-		$header->Assign('app_title', APP_TITLE);
-		$header->Assign('brand', APP_NAME);
-		$header->Assign('user', isset($_SESSION['nama']) ? $_SESSION['nama'] : '');
-		$header->Assign('tabmenu', 'beranda');
-		$this->Assign('header', $header->Render('header', false));
+    function __construct() {
+        parent::__construct();
+        $header = new View();
+        $header->Assign('wewenang', $_SESSION['wewenang']);
+        $header->Assign('app_title', APP_TITLE);
+        $header->Assign('brand', APP_NAME);
+        $header->Assign('user', isset($_SESSION['nama']) ? $_SESSION['nama'] : '');
+        $header->Assign('tabmenu', '');
+        $this->Assign('header', $header->Render('header', false));
 
-		$footer = new View();
-		$this->Assign('footer', $header->Render('footer', false));
+        $footer = new View();
+        $this->Assign('footer', $header->Render('footer', false));
 
-		// $side_banner = !isset($_SESSION['id']) ? [] : [];
-		$module_sidebanner = new Module([]);
-		$this->Assign('side_banner', $module_sidebanner->Render());
-		$module_left = new Module( /*['karir-side','links-side']*/);
-		$this->Assign('module_left', $module_left->Render());
-		$module_right = new Module( /*['populer','forum-side']*/);
-		$this->Assign('module_right', $module_right->Render());
-		$module_banner = new Module( /*['banner']*/);
-		$this->Assign('module_banner', $module_banner->Render());
-		$module_main = new Module( /*['artikel-terbaru']*/);
-		$this->Assign('module_main', $module_main->Render());
-	}
-	public function index() {
-		$peserta = new Peserta;
-		$peserta->getProfile($_SESSION['id'], 'D');
+        // $side_banner = !isset($_SESSION['id']) ? [] : [];
+        $module_sidebanner = new Module([]);
+        $this->Assign('side_banner', $module_sidebanner->Render());
+        $module_left = new Module( /*['karir-side','links-side']*/);
+        $this->Assign('module_left', $module_left->Render());
+        $module_right = new Module( /*['populer','forum-side']*/);
+        $this->Assign('module_right', $module_right->Render());
+        $module_banner = new Module( /*['banner']*/);
+        $this->Assign('module_banner', $module_banner->Render());
+        $module_main = new Module( /*['artikel-terbaru']*/);
+        $this->Assign('module_main', $module_main->Render());
+    }
+    public function index() {
+        $peserta = new Peserta;
+        $peserta->getProfile($_SESSION['id'], 'D');
 
-		$pendaftaran = new Pendaftaran;
-		$result = $pendaftaran->getPendaftaran($peserta);
-		if (empty($result)) {
-			$this->Load_View('error');
-			return;
-		}
-		$this->Load_View('seleksi/index');
-	}
+        $pendaftaran = new Pendaftaran;
+        $result = $pendaftaran->getPendaftaran($peserta);
+        if (empty($result)) {
+            session_destroy();
+            $this->Load_View('error');
+            return;
+        }
+        $this->Assign('peserta', $peserta);
+        $this->Load_View('seleksi/index');
+    }
 }
--- vendor/admin-daftarpeserta/admin-daftarpeserta.php
+++ vendor/admin-daftarpeserta/admin-daftarpeserta.php
@@ -3,27 +3,26 @@
  *
  */
 class AdminDaftarpeserta extends Module {
-	function __construct() {
-		$seleksi = new Seleksi;
-		$this->pesertas = $seleksi->getPesertas('D');
-	}
-	public function detail($id) {
-		echo 'test';
-		// include MODULE . strtolower('admin-daftarpeserta') . DS . 'test.php';
-		$v = new View;
-		include $this->Render(strtolower('admin-daftarpeserta') . DS . 'test.php');
-	}
-	public function block() {
+    function __construct() {
+        $seleksi = new Seleksi;
+        $this->pesertas = $seleksi->getPesertas('D');
+    }
+    public function detail($id) {
+        // include MODULE . strtolower('admin-daftarpeserta') . DS . 'test.php';
+        $v = new View;
+        include $this->Render(strtolower('admin-daftarpeserta') . DS . 'test.php');
+    }
+    public function block() {
 
-	}
-	public function hapus($id) {
+    }
+    public function hapus($id) {
 
-	}
-	// public function ubah($idartikel) {
-	// 	$data = $this->artikel->show($idartikel);
-	// 	var_dump($data);
-	// 	$this->judul = $data[0]->judul;
-	// 	$this->idkategori = $data[0]->idkategori;
-	// 	$this->isi = $data[0]->isi;
-	// }
+    }
+    // public function ubah($idartikel) {
+    //     $data = $this->artikel->show($idartikel);
+    //     var_dump($data);
+    //     $this->judul = $data[0]->judul;
+    //     $this->idkategori = $data[0]->idkategori;
+    //     $this->isi = $data[0]->isi;
+    // }
 }
